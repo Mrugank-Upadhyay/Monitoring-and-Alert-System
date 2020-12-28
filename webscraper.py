41d80d36bab8dde5c4126555f8db2dcb629297ef
Mrugank <mrugank2@gmail.com>
2020-12-28 17:45:54 -0500

    searching for availability works now

--- webscraper.py
+++ webscraper.py
@@ -2,17 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+import time
+
 # we will be using chrome
 # driver = webdriver.Chrome("/usr/bin/chromedriver")
+tempLink = "https://www.bestbuy.ca/en-ca/product/asus-tuf-24-fhd-144hz-1ms-gtg-ips-led-freesync-gaming-monitor-vg249q-black/14405119"
+
+
+link = input("Enter input Link: ")
 driver = webdriver.Chrome()
-# try:
-driver.get("https://www.bestbuy.ca/en-ca/product/asus-tuf-24-fhd-144hz-1ms-gtg-ips-led-freesync-gaming-monitor-vg249q-black/14405119")
-# productPriceContainer = driver.find_element_by_class_name(
-#     "productPricingContainer_3gTS3")
-# print(productPriceContainer.text)
+driver.get(link)
+
+# We're now going to find the availability tag which is a link tag with an href
 availability = driver.find_element(
-    By.XPATH, "//link[contains(@href, 'http://schema.org')]").value_of_css_property('href')
-print(availability)
-# except:
-#     pass
+    By.XPATH, '//link[contains(@href, "http://schema.org")]').get_attribute("href")
+
+if "InStock" in availability:
+    # we now have to send ourselves an email notification about the availability
+    print("I'm in stock!")
+
+elif "OutOfStock" in availability:
+    print("Whoops, I'm out of stock!")
+
 driver.quit()
